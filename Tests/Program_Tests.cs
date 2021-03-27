3dc553fb43f32ad13189533c5c8c9ba2537ca597
Martin Rudat <mrudat@toraboka.com>
2021-03-27 18:43:24 +1100

    test: test nearly the whole thing.

--- Tests/Program_Tests.cs
+++ Tests/Program_Tests.cs
@@ -1,7 +1,9 @@
 using GeneralStoresIngredientsPatcher;
 using Mutagen.Bethesda;
+using Mutagen.Bethesda.FormKeys.SkyrimSE;
 using Mutagen.Bethesda.Skyrim;
 using Mutagen.Bethesda.Synthesis;
+using System.Linq;
 using Xunit;
 
 namespace Tests
@@ -25,6 +27,57 @@ namespace Tests
             var program = new Program(loadOrder, linkCache, patchMod, GameRelease.SkyrimSE, new());
 
             program.RunPatch();
+        }
+
+        [Fact]
+        public static void ClassifyAThing()
+        {
+            var theItemFormLink = Skyrim.Ingestible.Ale;
+
+            SkyrimMod masterMod = new(theItemFormLink.FormKey.ModKey, SkyrimRelease.SkyrimSE);
+
+            var theItem = new Ingestible(theItemFormLink.FormKey, masterMod.SkyrimRelease);
+
+            masterMod.Ingestibles.Add(theItem);
+
+            ModKey modKey = ModKey.FromNameAndExtension("Patch.esp");
+
+            ISkyrimMod patchMod = new SkyrimMod(modKey, SkyrimRelease.SkyrimSE);
+
+            var formListLink = GeneralStores.FormList.xGSxAlchCookFLST;
+            var flst = new FormList(formListLink.FormKey, patchMod.SkyrimRelease)
+            {
+                EditorID = nameof(GeneralStores.FormList.xGSxAlchCookFLST)
+            };
+            patchMod.FormLists.Add(flst);
+
+            var newRecipe = patchMod.ConstructibleObjects.AddNew("newRecipe");
+            (newRecipe.Items ??= new()) .Add(new()
+            {
+                Item = new()
+                {
+                    Item = theItemFormLink
+                }
+            });
+            newRecipe.WorkbenchKeyword.SetTo(Skyrim.Keyword.CraftingCookpot);
+
+            LoadOrder<IModListing<ISkyrimModGetter>> loadOrder = new()
+            {
+                new ModListing<ISkyrimMod>(masterMod, true),
+                new ModListing<ISkyrimMod>(patchMod, true)
+            };
+
+            ILinkCache<ISkyrimMod, ISkyrimModGetter> linkCache = loadOrder.ToImmutableLinkCache();
+
+            var program = new Program(loadOrder, linkCache, patchMod, GameRelease.SkyrimSE, new());
+
+
+            program.RunPatch();
+
+
+            var itemLink = patchMod.FormLists.Single().Items.Single();
+
+            Assert.Equal(theItemFormLink, itemLink.Cast<IIngestibleGetter>());
         }
     }
 }
